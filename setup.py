83f2d508272c7decfa87aa683c11adfe2622da5c
Ayush <ayushpriya10@gmail.com>
2023-04-24 16:03:08 +0530

    Updated tool version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='IMDShift',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
